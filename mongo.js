a07f7df0b08c7948b789a92665bc9912f24c7ce1
Mian Nouman Ahmed <numan1836@gmail.com>
2023-11-19 21:22:47 +0500

    Initial commit

---
+++ mongo.js
@@ -0,0 +1,43 @@
+const MongoClient = require("mongodb").MongoClient;
+
+const url =
+  "mongodb+srv://numan1836:cricket1836@cluster0.mdpeqy7.mongodb.net/products_test?retryWrites=true&w=majority";
+
+const createProduct = async (req, res, next) => {
+  const newProduct = {
+    name: req.body.name,
+    price: req.body.price,
+  };
+  const client = new MongoClient(url);
+
+  try {
+    await client.connect();
+    const db = client.db();
+    const result = await db.collection("products").insertOne(newProduct);
+  } catch (error) {
+    return res.json({ message: "Could not store data." });
+  }
+  client.close();
+
+  res.json(newProduct);
+};
+
+const getProducts = async (req, res, next) => {
+  const client = new MongoClient(url);
+
+  let products;
+
+  try {
+    await client.connect();
+    const db = client.db();
+    products = await db.collection("products").find().toArray();
+  } catch (error) {
+    return res.json({ message: "Could not Retrive Products." });
+  }
+  client.close();
+
+  res.json(products);
+};
+
+exports.createProduct = createProduct;
+exports.getProducts = getProducts;
